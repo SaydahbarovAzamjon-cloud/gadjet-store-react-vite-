72dca954da6245a148f8ab22d66f85126676e213
Azamjon <iamsaidakbarov@gmail.com>
2026-07-09 07:25:17 +0900

    fix: modified deploy.sh command

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ yarn run build
 # Eski (buzuq) jarayonni tozalab, PM2 ning o'z static-serveri bilan ishga tushiramiz.
 # --spa: React Router uchun har yo'lni index.html ga yo'naltiradi (serve/yarn kerak emas).
 pm2 delete GADJET-REACT 2>/dev/null || true
-pm2 serve dist 80 --spa --name GADJET-REACT
+pm2 serve dist 205 --spa --name GADJET-REACT
